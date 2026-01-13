6fc7b29e848f88cc4b9a712b7eb8a839a2771f6e
Acer <natsuaryal13@gmail.com>
2026-01-13 17:32:47 +0545

    feat: compare 2 number

---
+++ lab report programs/program27.c
@@ -0,0 +1,20 @@
+//Program to compare two numbers.
+
+#include <stdio.h>
+
+int main() {
+    float num1, num2;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    if (num1 > num2) {
+        printf("%.2f is greater than %.2f\n", num1, num2);
+    } else if (num1 < num2) {
+        printf("%.2f is smaller than %.2f\n", num1, num2);
+    } else {
+        printf("Both numbers are equal: %.2f = %.2f\n", num1, num2);
+    }
+
+    return 0;
+}
