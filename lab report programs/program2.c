e2a2c951ad9650cdd48c9d2c22090144b4865358
Acer <natsuaryal13@gmail.com>
2026-01-13 16:57:54 +0545

    feat: add program2: operations of two whole number

---
+++ lab report programs/program2.c
@@ -0,0 +1,22 @@
+// 2. Program to add, subtract, multiply, and divide two whole numbers.
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two whole numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Addition = %d\n", a + b);
+    printf("Subtraction = %d\n", a - b);
+    printf("Multiplication = %d\n", a * b);
+
+    if (b != 0) {
+        printf("Division = %d\n", a / b);
+    } else {
+        printf("Division not possible (division by zero)\n");
+    }
+
+    return 0;
+}
