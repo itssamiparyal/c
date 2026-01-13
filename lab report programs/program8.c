d18a22b0cf2548b503f11e03fecd69a4cd9f1253
Acer <natsuaryal13@gmail.com>
2026-01-13 17:06:23 +0545

    feat: area and perimeter of a rectangle

---
+++ lab report programs/program8.c
@@ -0,0 +1,17 @@
+// Program to find area and perimeter of a rectangle.
+#include <stdio.h>
+
+int main() {
+    float length, breadth, area, perimeter;
+
+    printf("Enter length and breadth: ");
+    scanf("%f %f", &length, &breadth);
+
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    printf("Area of rectangle = %.2f\n", area);
+    printf("Perimeter of rectangle = %.2f\n", perimeter);
+
+    return 0;
+}
