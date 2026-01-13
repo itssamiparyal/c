bf74c647956b716c738c655305e9417b0a5e1ee3
Acer <natsuaryal13@gmail.com>
2026-01-13 16:54:17 +0545

    feat: add program 1: display name using characters

---
+++ lab report programs/program1.c
@@ -0,0 +1,13 @@
+// Program to display your name using characters.
+
+#include<stdio.h>
+
+int main(){
+    char name[20];
+
+    printf("Enter your name: ");
+    scanf("%s",name);
+
+    printf("your name is %s\n",name);
+    return 0;
+}
