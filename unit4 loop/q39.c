40a5ac36462fd15ffb0b904dd5fa78125a81954b
Acer <natsuaryal13@gmail.com>
2026-01-26 12:03:08 +0545

    feat: harmonic series

--- unit4 loop/q39.c
+++ unit4 loop/q39.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
-int main{
+int main(){
     int i,n;
-    float sum;
+    float sum = 0;
     printf("Enter n:\n");
     scanf("%d",&n);
     for(i = 1 ; i <= n; i++){
         sum = sum + 1 /(float) i;
-    } 
+    }
+    printf("Sum =f harmonic series = %.2f",sum);
+    return 0;
 }
