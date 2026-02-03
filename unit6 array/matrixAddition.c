76b9179798f4952d01e087a14e15f1788dcc870b
Acer <natsuaryal13@gmail.com>
2026-02-03 11:34:15 +0545

    feat: 2d matrix addition

---
+++ unit6 array/matrixAddition.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#define ROWS 20
+#define COLS 20
+
+int main(){
+    int a[ROWS][COLS],b[ROWS][COLS],c[ROWS][COLS],i,j,p,q;
+    printf("Enter the size of all three matrices:(pXq)\n");
+    scanf("%dx%d",&p,&q);
+
+    printf("Enter the values in matrix a:\n");
+    for(i = 0;i<p;i++){
+        for(j=0;j<q;j++){
+            printf("Enter a[%d][%d]= ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("Enter the values in matrix b:\n");
+    for(i = 0;i<p;i++){
+        for(j=0;j<q;j++){
+            printf("Enter b[%d][%d]= ",i,j);
+            scanf("%d",&b[i][j]);
+        }
+    }
+
+    //addition process
+    for(i = 0;i<p;i++){
+        for(j=0;j<q;j++){
+            c[i][j] = a[i][j]+b[i][j];
+        }
+    }
+
+    printf("\nThe addition Matrix is :\n");
+    for(i = 0;i<p;i++){
+        for(j=0;j<q;j++){
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+}
