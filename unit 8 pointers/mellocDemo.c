00dc1e2b3e489e986e79d42cf182c601bcad0997
Acer <natsuaryal13@gmail.com>
2026-03-15 11:43:10 +0545

    feat:sorting

--- unit 8 pointers/mellocDemo.c
+++ unit 8 pointers/mellocDemo.c
@@ -1,21 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sorting(int *p, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            int temp;
+            if (*(p + i) > *(p + j))
+            {
+                temp = *(p + i);
+                *(p + i) = *(p + j);
+                *(p + j) = temp;
+            }
+        }
+    }
+}
+
 int main()
 {
     int *ptr, n;
+
     printf("How many blocks:\n");
     scanf("%d", &n);
+
     ptr = (int *)malloc(n * sizeof(int));
+
     for (int i = 0; i < n; i++)
     {
-        printf("enter %d element:", i + 1);
+        printf("Enter %d element: ", i + 1);
         scanf("%d", ptr + i);
     }
-    printf("\n");
+
+    printf("\nOriginal array:\n");
     for (int i = 0; i < n; i++)
     {
         printf("%d ", *(ptr + i));
     }
+
+    printf("\nSorting...\n");
+
+    sorting(ptr, n);
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", *(ptr + i));
+    }
+
+    free(ptr);
+
     return 0;
 }
