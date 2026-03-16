42d029e3737181c9ed21c9cd48d2c8152e130226
Acer <natsuaryal13@gmail.com>
2026-03-16 11:07:36 +0545

    feat:mellocDemo

--- unit 8 pointers/mellocDemo.c
+++ unit 8 pointers/mellocDemo.c
@@ -1,3 +1,65 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// void sorting(int *p, int size)
+// {
+//     for (int i = 0; i < size - 1; i++)
+//     {
+//         for (int j = i + 1; j < size; j++)
+//         {
+//             int temp;
+//             if (*(p + i) > *(p + j))
+//             {
+//                 temp = *(p + i);
+//                 *(p + i) = *(p + j);
+//                 *(p + j) = temp;
+//             }
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     int *ptr, n;
+
+//     printf("How many blocks:\n");
+//     scanf("%d", &n);
+
+//     ptr = (int *)malloc(n * sizeof(int));
+//     int *temp;
+//     for (temp = ptr; temp < ptr + n; temp++)
+//     {
+//         printf("enter the value at %u element:", temp);
+//         scanf("%d", temp);
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("Enter %d element: ", i + 1);
+//         scanf("%d", ptr + i);
+//     }
+
+//     printf("\nOriginal array:\n");
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", *(ptr + i));
+//     }
+
+//     printf("\nSorting...\n");
+
+//     sorting(ptr, n);
+
+//     printf("Sorted array:\n");
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", *(ptr + i));
+//     }
+
+//     free(ptr);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,10 +69,9 @@ void sorting(int *p, int size)
     {
         for (int j = i + 1; j < size; j++)
         {
-            int temp;
             if (*(p + i) > *(p + j))
             {
-                temp = *(p + i);
+                int temp = *(p + i);
                 *(p + i) = *(p + j);
                 *(p + j) = temp;
             }
@@ -22,14 +83,21 @@ int main()
 {
     int *ptr, n;
 
-    printf("How many blocks:\n");
+    printf("How many elements: ");
     scanf("%d", &n);
 
-    ptr = (int *)malloc(n * sizeof(int));
+    ptr = malloc(n * sizeof(int));
 
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    // Taking input
     for (int i = 0; i < n; i++)
     {
-        printf("Enter %d element: ", i + 1);
+        printf("Enter element %d: ", i + 1);
         scanf("%d", ptr + i);
     }
 
